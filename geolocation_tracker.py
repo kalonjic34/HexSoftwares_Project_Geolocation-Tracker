91a306a88979c77d1a543c4595e904f6b49f809c
Christian <kalonjic34@gmail.com>
2025-10-22 13:59:04 +0200

    Implement IP tracker - Get public IP

--- geolocation_tracker.py
+++ geolocation_tracker.py
@@ -1,1 +1,14 @@
+import requests
+IP_API = "https://api.ipify.org?format=json"
 
+def get_public_ip() -> str:
+    r = requests.get(IP_API, timeout=10)
+    r.raise_for_status()
+    return r.json().get("ip")
+
+def main():
+    ip = get_public_ip()
+    print(ip)
+
+if __name__ == "__main__":
+    main()
